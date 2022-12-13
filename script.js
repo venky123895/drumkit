fdeaf4292f348438ec853872f8f8ba509e281ed4
unknown <devathotivenkataramana@gmail.com>
2022-12-13 18:07:27 +0530

    adding files

---
+++ script.js
@@ -0,0 +1,38 @@
+const button1=document.getElementById('box1');
+const button2=document.getElementById('box2');
+const button3=document.getElementById('box3');
+const button4=document.getElementById('box4');
+const button5=document.getElementById('box5');
+const button6=document.getElementById('box6');
+const button7=document.getElementById('box7');
+
+
+
+button1.addEventListener('click',function(){
+    var sound1 = new Audio("sound1.wav");
+    sound1.play();
+})
+button2.addEventListener('click',function(){
+    var sound2 = new Audio("sound2.wav");
+    sound2.play();
+})
+button3.addEventListener('click',function(){
+    var sound3 = new Audio("sound3.wav");
+    sound3.play();
+})
+button4.addEventListener('click',function(){
+    var sound4 = new Audio("sound4.wav");
+    sound4.play();
+})
+button5.addEventListener('click',function(){
+    var sound5 = new Audio("sound5.wav");
+    sound5.play();
+})
+button6.addEventListener('click',function(){
+    var sound6 = new Audio("sound6.wav");
+    sound6.play();
+})
+button7.addEventListener('click',function(){
+    var sound7 = new Audio("sound7.wav");
+    sound7.play();
+})
